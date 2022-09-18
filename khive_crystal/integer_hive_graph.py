4efed306407519a43a79343ba98c6b44010e2315
snrsw <ale.nnn.r@gmail.com>
2022-09-18 21:06:50 +0900

    Updata variable name

--- khive_crystal/integer_hive_graph.py
+++ khive_crystal/integer_hive_graph.py
@@ -33,16 +33,16 @@ class IntegerHiveGraph:
     alpha: List[int]
     beta: List[int]
     gamma: List[int]
-    uij: List[List[int]]
+    Uij: List[List[int]]
 
     def __post_init__(self):
         object.__setattr__(self, "alpha", align_length(n=self.n, edge=self.alpha))
         object.__setattr__(self, "beta", align_length(n=self.n, edge=self.beta))
         object.__setattr__(self, "gamma", align_length(n=self.n, edge=self.gamma))
-        object.__setattr__(self, "full_uij", self.add_uii())
+        object.__setattr__(self, "full_Uij", self.add_Uii())
         self.is_integer_hive_graph()
 
-    def compute_uii(self, i: int) -> int:
+    def compute_Uii(self, i: int) -> int:
         """compute U_{ii}
             U_{ii} := beta_{i} - sum_{k=1}^{i-1}U_{ki}
 
@@ -57,27 +57,27 @@ class IntegerHiveGraph:
             >>> alpha: List[int] = [1, 1, 0]
             >>> beta: List[int] = [1, 1, 0]
             >>> gamma: List[int] = [0, 0, 0]
-            >>> uij: List[List[int]] = [[0, 0], [0]]
-            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, uij=uij)
-            >>> H.compute_uii(i=1)
+            >>> Uij: List[List[int]] = [[0, 0], [0]]
+            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.compute_Uii(i=1)
             1
 
             >>> n: int = 3
             >>> alpha: List[int] = [3, 3, 0]
             >>> beta: List[int] = [2, 3, 1]
             >>> gamma: List[int] = [0, 0, 0]
-            >>> uij: List[List[int]] = [[1, 0], [1]]
-            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, uij=uij)
-            >>> H.compute_uii(i=2)
+            >>> Uij: List[List[int]] = [[1, 0], [1]]
+            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.compute_Uii(i=2)
             2
 
             >>> n: int = 3
             >>> alpha: List[int] = [3, 1, 0]
             >>> beta: List[int] = [1, 1, 2]
             >>> gamma: List[int] = [0, 0, 0]
-            >>> uij: List[List[int]] = [[1, 1], [1]]
-            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, uij=uij)
-            >>> H.compute_uii(i=3)
+            >>> Uij: List[List[int]] = [[1, 1], [1]]
+            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.compute_Uii(i=3)
             0
         """
 
@@ -90,8 +90,8 @@ class IntegerHiveGraph:
             self.beta[i_as_index] - sum(uji[i_as_index - 1]) if 1 < i else self.beta[0]
         )
 
-    def add_uii(self) -> List[List[int]]:
-        """Add uii to uij
+    def add_Uii(self) -> List[List[int]]:
+        """Add Uii to Uij
 
         Returns:
             List[List[int]]: [U_{ii}, U_{i,i+1}, ...]
@@ -101,18 +101,18 @@ class IntegerHiveGraph:
             >>> alpha: List[int] = [1, 1, 1]
             >>> beta: List[int] = [1, 1, 1]
             >>> gamma: List[int] = [0, 0, 0]
-            >>> uij: List[List[int]] = [[0, 0], [0]]
-            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, uij=uij)
-            >>> H.add_uii()
+            >>> Uij: List[List[int]] = [[0, 0], [0]]
+            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.add_Uii()
             [[1, 0, 0], [1, 0], [1]]
         """
-        return [[self.compute_uii(i=i + 1)] + ui for i, ui in enumerate(self.uij)] + [
-            [self.compute_uii(i=self.n - 1)]
+        return [[self.compute_Uii(i=i + 1)] + ui for i, ui in enumerate(self.Uij)] + [
+            [self.compute_Uii(i=self.n - 1)]
         ]
 
     def get_uji(self) -> List[List[int]]:
-        """get uji by aligning uij.
-        Let uij = [[U_{12}, U_{13}, U_{14}], [U_{23}, U_{24}], [U_{34}]].
+        """get uji by aligning Uij.
+        Let Uij = [[U_{12}, U_{13}, U_{14}], [U_{23}, U_{24}], [U_{34}]].
         Then uji = [[U_{12}], [U_{13}, U_{23}], [U_{14}, U_{24}], [U_{34}]].
 
         Returns:
@@ -123,8 +123,8 @@ class IntegerHiveGraph:
             >>> alpha: List[int] = [2, 1, 0]
             >>> beta: List[int] = [2, 1, 0]
             >>> gamma: List[int] = [0, 0, 0]
-            >>> uij: List[List[int]] = [[0, 0], [0]]
-            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, uij=uij)
+            >>> Uij: List[List[int]] = [[0, 0], [0]]
+            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
             >>> H.get_uji()
             [[0], [0, 0]]
 
@@ -132,8 +132,8 @@ class IntegerHiveGraph:
             >>> alpha: List[int] = [2, 1, 0]
             >>> beta: List[int] = [0, 2, 1]
             >>> gamma: List[int] = [0, 0, 0]
-            >>> uij: List[List[int]] = [[2, 0], [1]]
-            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, uij=uij)
+            >>> Uij: List[List[int]] = [[2, 0], [1]]
+            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
             >>> H.get_uji()
             [[2], [0, 1]]
 
@@ -141,16 +141,16 @@ class IntegerHiveGraph:
             >>> alpha: List[int] = [3, 1, 0]
             >>> beta: List[int] = [1, 1, 2]
             >>> gamma: List[int] = [0, 0, 0]
-            >>> uij: List[List[int]] = [[1, 1], [1]]
-            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, uij=uij)
+            >>> Uij: List[List[int]] = [[1, 1], [1]]
+            >>> H: IntegerHiveGraph = IntegerHiveGraph(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
             >>> H.get_uji()
             [[1], [1, 1]]
         """
 
-        aligned_uij: List[List[int]] = [
-            [0] * ((self.n - 1) - len(ui)) + ui for ui in self.uij
+        aligned_Uij: List[List[int]] = [
+            [0] * ((self.n - 1) - len(ui)) + ui for ui in self.Uij
         ]
-        return [list(uj)[: j + 1] for j, uj in enumerate(zip(*aligned_uij))]
+        return [list(uj)[: j + 1] for j, uj in enumerate(zip(*aligned_Uij))]
 
     def integer_hive_graph_condition(self, k: int) -> bool:
         """Compute the condition of integer hive graph:
@@ -166,7 +166,7 @@ class IntegerHiveGraph:
         k_as_index: int = k - 1
         is_hold: bool
         if k == 1:
-            is_hold = self.beta[0] == (self.gamma[0] + self.alpha[0] - sum(self.uij[0]))
+            is_hold = self.beta[0] == (self.gamma[0] + self.alpha[0] - sum(self.Uij[0]))
         elif k == self.n:
             is_hold = self.beta[k_as_index] == (
                 self.gamma[k_as_index]
@@ -178,7 +178,7 @@ class IntegerHiveGraph:
                 self.gamma[k_as_index]
                 + sum(self.get_uji()[k_as_index - 1])  # \sum_{l=1}^{k-1} U_{lk}
                 + self.alpha[k_as_index]
-                - sum(self.uij[k_as_index])
+                - sum(self.Uij[k_as_index])
             )  # \sum_{l=k+1}^{n} U_{kl}
         return is_hold
 
