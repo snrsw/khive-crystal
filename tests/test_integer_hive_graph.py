8675215c1708187268cc629889ce899a10eb6fa3
snrsw <ale.nnn.r@gmail.com>
2022-09-18 21:03:47 +0900

    Updata variable name

--- tests/test_integer_hive_graph.py
+++ tests/test_integer_hive_graph.py
@@ -2,19 +2,19 @@ import pytest
 from khive_crystal.integer_hive_graph import IntegerHiveGraph
 
 
-def test_fails_integer_hive_graph_invalid_uij():
+def test_fails_integer_hive_graph_invalid_Uij():
 
     with pytest.raises(ValueError) as e:
         IntegerHiveGraph(
-            n=3, alpha=[2, 1, 0], beta=[2, 1, 0], gamma=[0, 0, 0], uij=[[1, 0], [0]]
+            n=3, alpha=[2, 1, 0], beta=[2, 1, 0], gamma=[0, 0, 0], Uij=[[1, 0], [0]]
         )
 
 
-def test_passes_integer_hive_graph_invalid_uij():
+def test_passes_integer_hive_graph_invalid_Uij():
 
     assert isinstance(
         IntegerHiveGraph(
-            n=3, alpha=[2, 1, 0], beta=[2, 1, 0], gamma=[0, 0, 0], uij=[[0, 0], [0]]
+            n=3, alpha=[2, 1, 0], beta=[2, 1, 0], gamma=[0, 0, 0], Uij=[[0, 0], [0]]
         ),
         IntegerHiveGraph,
     )
