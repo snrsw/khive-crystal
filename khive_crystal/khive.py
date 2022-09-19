d4d1fb7f1ff16001097ebd04d049c1e47490039e
snrsw <ale.nnn.r@gmail.com>
2022-09-19 19:13:32 +0900

    add KHive

---
+++ khive_crystal/khive.py
@@ -0,0 +1,161 @@
+from dataclasses import dataclass
+from typing import List
+
+from khive_crystal.integer_hive_graph import IntegerHiveGraph
+
+
+@dataclass(frozen=True)
+class KHive(IntegerHiveGraph):
+    def __post_init__(self) -> None:
+        super().__post_init__()
+        self.is_k_hive()
+
+    def is_eq_edge_sum(self) -> bool:
+        """Check sum(alpha) == sum(beta)
+
+        Returns:
+            bool: sum(alpha) == sum(beta)
+
+        Examples:
+            >>> n: int = 3
+            >>> alpha: List[int] = [1, 1, 0]
+            >>> beta: List[int] = [1, 1, 0]
+            >>> gamma: List[int] = [0, 0, 0]
+            >>> Uij: List[List[int]] = [[0, 0], [0]]
+            >>> H: KHive = KHive(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.is_eq_edge_sum()
+            True
+        """
+        return sum(self.alpha) == sum(self.beta)
+
+    def is_partition(self) -> bool:
+        """Check alpha is a partition.
+
+        Returns:
+            bool: If alpha is partition, return True.
+
+        Examples:
+            >>> n: int = 3
+            >>> alpha: List[int] = [1, 1, 0]
+            >>> beta: List[int] = [1, 1, 0]
+            >>> gamma: List[int] = [0, 0, 0]
+            >>> Uij: List[List[int]] = [[0, 0], [0]]
+            >>> H: KHive = KHive(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.is_partition()
+            True
+        """
+
+        return all(x >= y for x, y in zip(self.alpha, self.alpha[1:]))
+
+    def is_zero_partition(self) -> bool:
+        """Check gamma is 0
+
+        Returns:
+            bool: If gamma = (0^n), return True.
+
+        Examples:
+            >>> n: int = 3
+            >>> alpha: List[int] = [1, 1, 0]
+            >>> beta: List[int] = [1, 1, 0]
+            >>> gamma: List[int] = [0, 0, 0]
+            >>> Uij: List[List[int]] = [[0, 0], [0]]
+            >>> H: KHive = KHive(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.is_zero_partition()
+            True
+        """
+
+        return (sum(self.gamma) == 0) and all(x >= 0 for x in self.gamma)
+
+    def is_Uij_geq_zero(self) -> bool:
+        """Check U_{ij} >= 0.
+
+        Returns:
+            bool: If U_{ij} >= 0, return True.
+
+        Examples:
+            >>> n: int = 3
+            >>> alpha: List[int] = [1, 1, 0]
+            >>> beta: List[int] = [1, 1, 0]
+            >>> gamma: List[int] = [0, 0, 0]
+            >>> Uij: List[List[int]] = [[0, 0], [0]]
+            >>> H: KHive = KHive(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.is_Uij_geq_zero()
+            True
+        """
+        return all(x >= 0 for x in sum(self.Uij, []))
+
+    def Lij(self, i: int, j: int) -> int:
+        """Compute L_{ij} defined by
+            L_{ij} = sum_{k=1}^{j-1}U_{ik} - sum_{k=1}^{j}U_{i+1,k}.
+
+        Args:
+            i (int): i in [n]
+            j (int): j in [i+1, n]_{mathbb{Z}}
+
+        Returns:
+            int: L_{ij}
+
+        Raises:
+            ValueError: If (i, j) dose not satisfy 1 <= i < j <= n, raise error.
+
+        Examples:
+            >>> n: int = 3
+            >>> alpha: List[int] = [4, 2, 0]
+            >>> beta: List[int] = [2, 1, 3]
+            >>> gamma: List[int] = [0, 0, 0]
+            >>> Uij: List[List[int]] = [[1, 1], [2]]
+            >>> H: KHive = KHive(n=n, alpha=alpha, beta=beta, gamma=gamma, Uij=Uij)
+            >>> H.Lij(i=1, j=2)
+            2
+        """
+
+        if not ((1 <= i <= self.n - 1) or (1 <= j <= self.n)):
+            raise ValueError("Invalid values! i and j must be in [n].")
+
+        if i >= j:
+            raise ValueError("Invalid values! i and j must be i < j.")
+
+        i_as_index: int = i - 1
+        j_as_index: int = j - 1
+        Uik: int = sum(self.full_Uij[i_as_index][: j_as_index - i_as_index])
+        Uip1k: int = (
+            sum(self.full_Uij[i_as_index + 1][: j_as_index - i_as_index - 1])
+            if i_as_index + 1 < self.n - 1
+            else 0
+        )
+        return Uik - Uip1k
+
+    def is_Lij_geq_zero(self) -> bool:
+        """Check L_{ij} >= 0
+
+        Returns:
+            bool: If all L_{ij} >= 0, return True.
+        """
+        return all(
+            self.Lij(i=i + 1, j=j + 1) >= 0
+            for i in range(self.n - 1)
+            for j in range(i + 1, self.n)
+        )
+
+    def is_Uii_geq_zero(self) -> bool:
+        return all(self.compute_Uii(i=i + 1) >= 0 for i in range(self.n))
+
+    def is_k_hive(self) -> None:
+        """Check given values saitisfy the definition of K-hive.
+
+        Raises:
+            ValueError: _description_
+        """
+        is_k_hive: bool = all(
+            [
+                self.is_eq_edge_sum(),
+                self.is_partition(),
+                self.is_Uij_geq_zero(),
+                self.is_Lij_geq_zero(),
+                self.is_Uii_geq_zero(),
+            ]
+        )
+        if not is_k_hive:
+            raise ValueError(
+                "Invalid values! Given values dose not satisfy the definition of K-hive"
+            )
