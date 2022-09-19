d948fbcb91adf130466e958e5e0d85a10e473a54
snrsw <ale.nnn.r@gmail.com>
2022-09-19 17:33:54 +0900

    Update and add tests

--- tests/test_k_hive.py
+++ tests/test_k_hive.py
@@ -3,8 +3,14 @@ from khive_crystal.k_hive import KHive
 
 
 def test_fails_k_hive():
-    with pytest.raises(ValueError) as e:
-        KHive(n=3, alpha=[2, 1, 0], beta=[2, 1, 0], gamma=[0, 0, 0], Uij=[[1, 0], [0]])
+    with pytest.raises(ValueError):
+        KHive(n=3, alpha=[1, 2, 0], beta=[1, 2, 0], gamma=[0, 0, 0], Uij=[[0, 0], [0]])
+
+    with pytest.raises(ValueError):
+        KHive(n=3, alpha=[3, 2, 0], beta=[3, 1, 0], gamma=[0, 1, 0], Uij=[[0, 0], [0]])
+
+    with pytest.raises(ValueError):
+        KHive(n=3, alpha=[3, 2, 0], beta=[3, 1, 0], gamma=[0, 0, 0], Uij=[[0, 0], [-1]])
 
 
 def test_passes_k_hive():
