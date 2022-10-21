25716a04d5e0dc6d6050b060d707d1f9a48774b0
snrsw <ale.nnn.r@gmail.com>
2022-10-21 22:06:15 +0900

    Fix __init__.py

--- khive_crystal/__init__.py
+++ khive_crystal/__init__.py
@@ -1,7 +1,7 @@
 from khive_crystal.crystal_structure import e, epsilon, f, phi
 from khive_crystal.decompositions import decompose, split
-from khive_crystal.khive import KHive
-from khive_crystal.khives import KHives
+from khive_crystal.khive import khive
+from khive_crystal.khives import khives
 from khive_crystal.view import view
 
-__all__ = ["decompose", "split", "KHive", "KHives", "view", "e", "epsilon", "f", "phi"]
+__all__ = ["decompose", "split", "khive", "khives", "view", "e", "epsilon", "f", "phi"]
