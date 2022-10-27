9da93a2744e8fe0406c6b3a3d5ff36f1501bde9a
snrsw <ale.nnn.r@gmail.com>
2022-10-28 03:06:14 +0900

    Fix bugs

--- khive_crystal/__init__.py
+++ khive_crystal/__init__.py
@@ -1,13 +1,13 @@
 from khive_crystal.crystal_graph import crystal_graph
 from khive_crystal.crystal_structure import e, epsilon, f, phi
-from khive_crystal.decompositions import decompose, split
+from khive_crystal.decompositions import psi, psi_lambda
 from khive_crystal.khive import khive
 from khive_crystal.khives import khives
 from khive_crystal.view import view
 
 __all__ = [
-    "decompose",
-    "split",
+    "psi",
+    "psi_lambda",
     "khive",
     "khives",
     "view",
